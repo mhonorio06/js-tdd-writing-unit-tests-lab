952e1c083059b6726049bc8a458e20cce7c2b6ee
Manny <mhfernan35277@gmail.com>
2024-03-12 23:32:15 -0700

    adds file

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
 // Your code here
+function reverse(string) {
+    return string.split("").reverse().join("");
+  }
+  export function isPalindrome(word) {
+    const lowerCaseWord = word.toLowerCase();
+    return lowerCaseWord === reverse(lowerCaseWord);
+  }
